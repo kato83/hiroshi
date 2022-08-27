9640b1ec0ef111c2b1a894f817b41fd52c949963
kato83 <katomeganeb83@gmail.com>
2022-08-27 18:02:00 +0900

    fix jsx namespace

--- src/hiroshi.ts
+++ src/hiroshi.ts
@@ -3,17 +3,13 @@
  * https://github.com/kato83/hiroshi/blob/master/LICENSE.txt
  */
 
-import type {createElement} from './jsx.d';
-
-export {createElement as h};
-
 /**
  * Build element.
  * @param nodeName element name or Component function
  * @param attributes attributes
  * @param children rest parameter children
  */
-const createElement = (
+export const createElement = (
   nodeName: string | ((...props) => (...props) => Element),
   attributes: { [p: string]: unknown } = {},
   ...children: any[]
@@ -39,6 +35,8 @@ const createElement = (
     return elm;
   };
 };
+
+export {createElement as h};
 
 const applyAttributes = (elm: Element, attributes: { [p: string]: unknown } = {}) => {
   const {ref, ...otherAttributes} = attributes ?? {};
@@ -115,3 +113,199 @@ const isMaybeEvent = (key: string) => key.startsWith('on') && isNotNullable(key[
  * convert camel case to kebab case.
  */
 const camel2KebabCase = (str: string) => str.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
+
+/**
+ * Apply from React TypeScript definition
+ * @see https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/index.d.ts
+ */
+export namespace createElement {
+  export namespace JSX {
+    export type Element = (namespace?: string) => Node;
+    export type Fragment = () => DocumentFragment;
+
+    export interface IntrinsicAttributes {
+      ref?: ((ref: JSX.Element) => {}) | {};
+    }
+
+    export interface IntrinsicElements {
+      // HTML
+      a: HTMLAnchorElement;
+      abbr: HTMLElement;
+      address: HTMLElement;
+      area: HTMLAreaElement;
+      article: HTMLElement;
+      aside: HTMLElement;
+      audio: HTMLAudioElement;
+      b: HTMLElement;
+      base: HTMLBaseElement;
+      bdi: HTMLElement;
+      bdo: HTMLElement;
+      big: HTMLElement;
+      blockquote: HTMLQuoteElement;
+      body: HTMLBodyElement;
+      br: HTMLBRElement;
+      button: HTMLButtonElement;
+      canvas: HTMLCanvasElement;
+      caption: HTMLElement;
+      cite: HTMLElement;
+      code: HTMLElement;
+      col: HTMLTableColElement;
+      colgroup: HTMLTableColElement;
+      data: HTMLDataElement;
+      datalist: HTMLDataListElement;
+      dd: HTMLElement;
+      del: HTMLModElement;
+      details: HTMLDetailsElement;
+      dfn: HTMLElement;
+      dialog: HTMLDialogElement;
+      div: HTMLDivElement;
+      dl: HTMLDListElement;
+      dt: HTMLElement;
+      em: HTMLElement;
+      embed: HTMLEmbedElement;
+      fieldset: HTMLFieldSetElement;
+      figcaption: HTMLElement;
+      figure: HTMLElement;
+      footer: HTMLElement;
+      form: HTMLFormElement;
+      h1: HTMLHeadingElement;
+      h2: HTMLHeadingElement;
+      h3: HTMLHeadingElement;
+      h4: HTMLHeadingElement;
+      h5: HTMLHeadingElement;
+      h6: HTMLHeadingElement;
+      head: HTMLElement;
+      header: HTMLElement;
+      hgroup: HTMLElement;
+      hr: HTMLHRElement;
+      html: HTMLHtmlElement;
+      i: HTMLElement;
+      iframe: HTMLIFrameElement;
+      img: HTMLImageElement;
+      input: HTMLInputElement;
+      ins: HTMLModElement;
+      kbd: HTMLElement;
+      keygen: HTMLElement;
+      label: HTMLLabelElement;
+      legend: HTMLLegendElement;
+      li: HTMLLIElement;
+      link: HTMLLinkElement;
+      main: HTMLElement;
+      map: HTMLMapElement;
+      mark: HTMLElement;
+      menu: HTMLElement;
+      menuitem: HTMLElement;
+      meta: HTMLMetaElement;
+      meter: HTMLMeterElement;
+      nav: HTMLElement;
+      noscript: HTMLElement;
+      object: HTMLObjectElement;
+      ol: HTMLOListElement;
+      optgroup: HTMLOptGroupElement;
+      option: HTMLOptionElement;
+      output: HTMLOutputElement;
+      p: HTMLParagraphElement;
+      param: HTMLParamElement;
+      picture: HTMLElement;
+      pre: HTMLPreElement;
+      progress: HTMLProgressElement;
+      q: HTMLQuoteElement;
+      rp: HTMLElement;
+      rt: HTMLElement;
+      ruby: HTMLElement;
+      s: HTMLElement;
+      samp: HTMLElement;
+      slot: HTMLSlotElement;
+      script: HTMLScriptElement;
+      section: HTMLElement;
+      select: HTMLSelectElement;
+      small: HTMLElement;
+      source: HTMLSourceElement;
+      span: HTMLSpanElement;
+      strong: HTMLElement;
+      style: HTMLStyleElement;
+      sub: HTMLElement;
+      summary: HTMLElement;
+      sup: HTMLElement;
+      table: HTMLTableElement;
+      template: HTMLTemplateElement;
+      tbody: HTMLTableSectionElement;
+      td: HTMLTableDataCellElement;
+      textarea: HTMLTextAreaElement;
+      tfoot: HTMLTableSectionElement;
+      th: HTMLTableHeaderCellElement;
+      thead: HTMLTableSectionElement;
+      time: HTMLTimeElement;
+      title: HTMLTitleElement;
+      tr: HTMLTableRowElement;
+      track: HTMLTrackElement;
+      u: HTMLElement;
+      ul: HTMLUListElement;
+      "var": HTMLElement;
+      video: HTMLVideoElement;
+      wbr: HTMLElement;
+      webview: HTMLElement;
+
+      // SVG
+      svg: SVGSVGElement;
+
+      animate: SVGElement;
+      animateMotion: SVGElement;
+      animateTransform: SVGElement;
+      circle: SVGCircleElement;
+      clipPath: SVGClipPathElement;
+      defs: SVGDefsElement;
+      desc: SVGDescElement;
+      ellipse: SVGEllipseElement;
+      feBlend: SVGFEBlendElement;
+      feColorMatrix: SVGFEColorMatrixElement;
+      feComponentTransfer: SVGFEComponentTransferElement;
+      feComposite: SVGFECompositeElement;
+      feConvolveMatrix: SVGFEConvolveMatrixElement;
+      feDiffuseLighting: SVGFEDiffuseLightingElement;
+      feDisplacementMap: SVGFEDisplacementMapElement;
+      feDistantLight: SVGFEDistantLightElement;
+      feDropShadow: SVGFEDropShadowElement;
+      feFlood: SVGFEFloodElement;
+      feFuncA: SVGFEFuncAElement;
+      feFuncB: SVGFEFuncBElement;
+      feFuncG: SVGFEFuncGElement;
+      feFuncR: SVGFEFuncRElement;
+      feGaussianBlur: SVGFEGaussianBlurElement;
+      feImage: SVGFEImageElement;
+      feMerge: SVGFEMergeElement;
+      feMergeNode: SVGFEMergeNodeElement;
+      feMorphology: SVGFEMorphologyElement;
+      feOffset: SVGFEOffsetElement;
+      fePointLight: SVGFEPointLightElement;
+      feSpecularLighting: SVGFESpecularLightingElement;
+      feSpotLight: SVGFESpotLightElement;
+      feTile: SVGFETileElement;
+      feTurbulence: SVGFETurbulenceElement;
+      filter: SVGFilterElement;
+      foreignObject: SVGForeignObjectElement;
+      g: SVGGElement;
+      image: SVGImageElement;
+      line: SVGLineElement;
+      linearGradient: SVGLinearGradientElement;
+      marker: SVGMarkerElement;
+      mask: SVGMaskElement;
+      metadata: SVGMetadataElement;
+      mpath: SVGElement;
+      path: SVGPathElement;
+      pattern: SVGPatternElement;
+      polygon: SVGPolygonElement;
+      polyline: SVGPolylineElement;
+      radialGradient: SVGRadialGradientElement;
+      rect: SVGRectElement;
+      stop: SVGStopElement;
+      switch: SVGSwitchElement;
+      symbol: SVGSymbolElement;
+      text: SVGTextElement;
+      textPath: SVGTextPathElement;
+      tspan: SVGTSpanElement;
+      use: SVGUseElement;
+      view: SVGViewElement;
+    }
+  }
+}
