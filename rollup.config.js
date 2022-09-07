f7676edeca3fc9847029f54728d6614bfb516071
kato83 <katomeganeb83@gmail.com>
2022-09-08 00:20:50 +0900

    remove unused package

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,5 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import {terser} from "rollup-plugin-terser";
-import babel from 'rollup-plugin-babel';
 
 const entry = type => ({
   input: 'src/hiroshi.ts',
@@ -16,26 +13,8 @@ const entry = type => ({
     entryFileNames: 'hiroshi.js',
   },
   plugins: [
-    // npmモジュールを`node_modules`から読み込む
-    nodeResolve({main: true}),
-    // CommonJSモジュールをES6に変換
     typescript({
       declarationDir: `dist/${type}`,
-    }),
-    commonjs(),
-    babel({
-      "presets": [
-        [
-          "@babel/preset-env",
-          {
-            "useBuiltIns": "usage",
-            "corejs": 3,
-            "targets": {
-              "ie": "11"
-            }
-          }
-        ]
-      ]
     }),
     terser({output: {comments: /@license/i}}),
   ]
